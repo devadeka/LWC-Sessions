fc023f2400e4eed92abbce0712c88d33cd571220
Devatanu Deka <deva.deka@gmail.com>
2020-10-02 08:00:17 +1000

    Section7 view record

--- force-app/main/default/lwc/accontManagerLDS/accontManagerLDS.js
+++ force-app/main/default/lwc/accontManagerLDS/accontManagerLDS.js
@@ -1,10 +1,17 @@
-import { LightningElement, track } from "lwc";
-import { createRecord } from "lightning/uiRecordApi";
+import { LightningElement, track, wire } from "lwc";
+import { createRecord, getRecord } from "lightning/uiRecordApi";
+
+const fieldArray = ["Account.Name", "Account.Phone", "Account.Website"];
 
 export default class AccontManagerLDS extends LightningElement {
   @track accountName;
   @track accountPhone;
   @track accountWebsite;
+
+  @track recordId;
+
+  @wire(getRecord, { recordId: "$recordId", fields: fieldArray })
+  accountRecord;
 
   accountNameChangeHandler(event) {
     this.accountName = event.target.value;
@@ -14,6 +21,27 @@ export default class AccontManagerLDS extends LightningElement {
   }
   accountWebsiteChangeHandler(event) {
     this.accountWebsite = event.target.value;
+  }
+
+  get retAccountName() {
+    if (this.accountRecord.data) {
+      return this.accountRecord.data.fields.Name.value;
+    }
+    return undefined;
+  }
+
+  get retAccountPhone() {
+    if (this.accountRecord.data) {
+      return this.accountRecord.data.fields.Phone.value;
+    }
+    return undefined;
+  }
+
+  get retAccountWebsite() {
+    if (this.accountRecord.data) {
+      return this.accountRecord.data.fields.Website.value;
+    }
+    return undefined;
   }
 
   createAccount() {
@@ -29,7 +57,10 @@ export default class AccontManagerLDS extends LightningElement {
     };
 
     createRecord(recordInput)
-      .then((data) => console.log("Account created wiht id:", data.id))
+      .then((data) => {
+        console.log("Account created wiht id:", data.id);
+        this.recordId = data.id;
+      })
       .catch((err) => console.log("Error:", err));
   }
 }
